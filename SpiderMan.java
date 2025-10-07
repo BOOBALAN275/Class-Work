d753eec55468dd9a4df404e86778d743d9400e55
BOOBALAN275 <boobalan272005@gmail.com>
2025-10-07 16:19:07 +0530

    New Class Works

---
+++ SpiderMan.java
@@ -0,0 +1,7 @@
+class SpiderMan
+{
+	public static void main(String[] args)
+	{
+		System.out.println("With Great Power Comes Great Responsiblity");
+	}
+}
---
+++ TheSandlot.java
@@ -0,0 +1,7 @@
+class TheSandlot
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Legends Never Die");
+	}
+}
